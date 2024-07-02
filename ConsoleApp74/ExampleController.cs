a9e18b13ac9bba093512edbda2705251936c46af
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-07-02 10:42:06 +0200

    Add project files.

---
+++ ConsoleApp74/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ConsoleApp74
+{
+    public class ExampleController : Controller
+    {
+        private static string TargetDirectory = "/path/to/target/directory/";
+
+        public void Example(string filename)
+        {
+            string path = Path.Combine(TargetDirectory, filename);
+            System.IO.File.Delete(path);
+        }
+    }
+}
